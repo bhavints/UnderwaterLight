c806c376b17484cf921a60eb5b88752aa932b7bd
bhavints <bhavints@usc.edu>
2021-04-19 21:38:25 -0700

    use new tri

--- Application5.cpp
+++ Application5.cpp
@@ -18,7 +18,7 @@ static char THIS_FILE[]=__FILE__;
 #define new DEBUG_NEW
 #endif
 
-#define INFILE  "ppot.asc"
+#define INFILE  "underwater.asc"
 #define OUTFILE "output.ppm"
 
 
@@ -114,7 +114,7 @@ GzMatrix	translate =
 	0.0,	0.0,	0.0,	1.0
 };
 
-#if 1 	/* set up app-defined camera if desired, else use camera defaults */
+#if 0 	/* set up app-defined camera if desired, else use camera defaults */
     camera.position[X] = -3;
     camera.position[Y] = -25;
     camera.position[Z] = -4;
